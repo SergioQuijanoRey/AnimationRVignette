03824bf7179872fbcdf80e6099d2fa7224beca60
Sergio <sergiquijano@gmail.com>
2022-06-24 13:08:44 +0200

    feat, fix: termino de arreglar todos los problemas del documento

--- js/k_means.js
+++ js/k_means.js
@@ -2,7 +2,7 @@
     $(document).ready(function() {
 	
 	$('#k_means').scianimator({
-	    'images': ['images/kmeans/k_means1.png', 'images/kmeans/k_means2.png', 'images/kmeans/k_means3.png', 'images/kmeans/k_means4.png', 'images/kmeans/k_means5.png', 'images/kmeans/k_means6.png', 'images/kmeans/k_means7.png', 'images/kmeans/k_means8.png', 'images/kmeans/k_means9.png', 'images/kmeans/k_means10.png', 'images/kmeans/k_means11.png', 'images/kmeans/k_means12.png', 'images/kmeans/k_means13.png', 'images/kmeans/k_means14.png', 'images/kmeans/k_means15.png', 'images/kmeans/k_means16.png', 'images/kmeans/k_means17.png', 'images/kmeans/k_means18.png', 'images/kmeans/k_means19.png', 'images/kmeans/k_means20.png', 'images/kmeans/k_means21.png', 'images/kmeans/k_means22.png', 'images/kmeans/k_means23.png', 'images/kmeans/k_means24.png', 'images/kmeans/k_means25.png', 'images/kmeans/k_means26.png', 'images/kmeans/k_means27.png', 'images/kmeans/k_means28.png', 'images/kmeans/k_means29.png', 'images/kmeans/k_means30.png', 'images/kmeans/k_means31.png', 'images/kmeans/k_means32.png', 'images/kmeans/k_means33.png', 'images/kmeans/k_means34.png', 'images/kmeans/k_means35.png', 'images/kmeans/k_means36.png'],
+	    'images': ['images/kmeans/k_means1.png', 'images/kmeans/k_means2.png', 'images/kmeans/k_means3.png', 'images/kmeans/k_means4.png', 'images/kmeans/k_means5.png', 'images/kmeans/k_means6.png'],
 	    'width': 480,
 	    'delay': 1000,
 	    'loopMode': 'loop'
