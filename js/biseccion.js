76d460dd111b4bc96a26db7f6e8f64bd7f01afec
Sergio <sergiquijano@gmail.com>
2022-06-24 12:51:37 +0200

    feat: intento arreglar los problemas con imgdir

--- js/biseccion.js
+++ js/biseccion.js
@@ -2,7 +2,7 @@
     $(document).ready(function() {
 	
 	$('#biseccion').scianimator({
-	    'images': ['images/biseccion1.png', 'images/biseccion2.png', 'images/biseccion3.png', 'images/biseccion4.png', 'images/biseccion5.png', 'images/biseccion6.png', 'images/biseccion7.png', 'images/biseccion8.png', 'images/biseccion9.png', 'images/biseccion10.png', 'images/biseccion11.png', 'images/biseccion12.png', 'images/biseccion13.png', 'images/biseccion14.png', 'images/biseccion15.png', 'images/biseccion16.png'],
+	    'images': ['images/biseccion/biseccion1.png', 'images/biseccion/biseccion2.png', 'images/biseccion/biseccion3.png', 'images/biseccion/biseccion4.png', 'images/biseccion/biseccion5.png', 'images/biseccion/biseccion6.png', 'images/biseccion/biseccion7.png', 'images/biseccion/biseccion8.png', 'images/biseccion/biseccion9.png', 'images/biseccion/biseccion10.png', 'images/biseccion/biseccion11.png', 'images/biseccion/biseccion12.png', 'images/biseccion/biseccion13.png', 'images/biseccion/biseccion14.png', 'images/biseccion/biseccion15.png', 'images/biseccion/biseccion16.png'],
 	    'width': 480,
 	    'delay': 1000,
 	    'loopMode': 'loop'
